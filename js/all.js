2f58bad599452ea0bfc335265fa8bad9d3d83484
amand <amand@Administrativo>
2019-04-15 09:21:31 -0300

    Mil e um Ajustes depois

--- js/all.js
+++ js/all.js
@@ -1044,3 +1044,12 @@ function split_height_init() {
 
 
 }
+$(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 50
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault()
+        })
+    });
